40aa90012ec5d5a216ec9cbe8546287227eaaf59
Sylver-Wolf <djfalcon37510@gmail.com>
2024-11-08 17:00:06 +0100

    Commit initiel du projet + exemples

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <FirestoreExample />
+    <AuthExemple />
+  </div>
+
+</template>
+
+<script>
+import FirestoreExample from './components/FirestoreExample.vue';
+import AuthExemple from './components/AuthExample.vue';
+
+export default {
+  name : "App",
+  component: {
+    FirestoreExample,
+    AuthExample,
+  }
+};
+</script>
+<style>
+/* Ajout des styles personnalisés ici si nécessaire*/
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'bulma/css/bulma.css'
+
+import { db, auth } from './firebase.js'
+
+const app = createApp(App)
+
+app.config.globalProperties.$db = db;
+app.config.globalProperties.$auth = auth;
+
+createApp(App).mount('#app')
