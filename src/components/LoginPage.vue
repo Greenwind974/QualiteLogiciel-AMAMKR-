912b03b269b2870a67c876318b72d4ee6508681d
Sylver-Wolf <djfalcon37510@gmail.com>
2024-11-15 17:03:07 +0100

    debut ajout style avec bulma

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,20 +1,20 @@
 <template>
-  <div>
+  <div class="is-flex is-flex-direction-column is-align-items-center">
     <h1>Bienvenue sur LocaMat !</h1>
     <form>
-      <div>
+      <div class="is-flex is-flex-direction-column">
         <label>Nom</label>
         <input placeholder="Entrez votre nom d'utilisateur" />
       </div>
-      <div>
+      <div class="is-flex is-flex-direction-column">
         <label>Adresse e-mail</label>
         <input type="email" v-model="email" placeholder="Entrez votre email" required />
       </div>
-      <div>
+      <div class="is-flex is-flex-direction-column">
         <label>Mot de passe</label>
         <input type="password" v-model="password" placeholder="Mot de passe" />
       </div>
-      <button>Connexion</button>
+      <button class="button is-link is-outlined">Connexion</button>
     </form>
   </div>
 </template>
