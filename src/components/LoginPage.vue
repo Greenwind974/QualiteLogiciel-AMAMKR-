f84df289f1a04ca99e26e5d537758d6d29510f17
Sylver-Wolf <djfalcon37510@gmail.com>
2024-11-15 16:44:57 +0100

    ajout du form (non fonctionnel) de login

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    <h1>DU TEXTE YEEPIE</h1>
+    <h1>Bienvenue sur LocaMat !</h1>
+    <form>
+      <div>
+        <label>Nom</label>
+        <input placeholder="Entrez votre nom d'utilisateur" />
+      </div>
+      <div>
+        <label>Adresse e-mail</label>
+        <input type="email" v-model="email" placeholder="Entrez votre email" required />
+      </div>
+      <div>
+        <label>Mot de passe</label>
+        <input type="password" v-model="password" placeholder="Mot de passe" />
+      </div>
+      <button>Connexion</button>
+    </form>
   </div>
 </template>
 
