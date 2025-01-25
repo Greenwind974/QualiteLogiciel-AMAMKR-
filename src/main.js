26e30cf8c5240c90cfd11dbc0244f434b027aa31
McSIM <mmarece17@gmail.com>
2025-01-25 10:28:56 +0100

    Installs nécessaires pour router et icones

--- src/main.js
+++ src/main.js
@@ -2,7 +2,17 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "bulma/css/bulma.css";
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import colors from 'vuetify/util/colors'
+
+
+import "@mdi/font/css/materialdesignicons.css"
+
 import { db, auth } from "./firebase.js";
+
 import router from "@/router/router";
 
 const app = createApp(App);
@@ -10,5 +20,37 @@ const app = createApp(App);
 app.config.globalProperties.$db = db;
 app.config.globalProperties.$auth = auth;
 app.use(router);
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: "mdi", // This is already the default value - only for display purposes
+    },
+    theme: {
+        themes: {
+            dark: {
+                light:false,
+                colors: {
+                    primary: colors.indigo.base,
+                    secondary: colors.indigo.lighten5,
+                    accent: colors.green.darken1,
+                    warning : colors.red.accent4,
+                }
+            },
+            light: {
+                dark:false,
+                colors: {
+                    primary: colors.indigo.base,
+                    secondary: colors.indigo.lighten5,
+                    accent: colors.green.darken1,
+                    warning : colors.red.accent4,
+                }
+            },
+        },
+    },
+})
+
+app.use(vuetify)
 
 app.mount("#app");
