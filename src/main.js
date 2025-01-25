dcda13a635ee9a79882f80d812d3ed4dc855d12d
waulyn <153715998+waulyn@users.noreply.github.com>
2025-01-25 10:52:08 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,8 +2,25 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "bulma/css/bulma.css";
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import colors from 'vuetify/util/colors'
+
+
+import "@mdi/font/css/materialdesignicons.css"
+
 import { db, auth } from "./firebase.js";
+
 import router from "@/router/router";
+import UpdateMaterial from "@/components/Material/UpdateMaterial.vue";
+import CreateMaterial from "@/components/Material/CreateMaterial.vue";
+import DeleteMaterial from "@/components/Material/DeleteMaterial.vue";
+import BookMaterial from "@/components/Material/BookMaterial.vue";
+
+
+
 
 const app = createApp(App);
 
@@ -11,4 +28,43 @@ app.config.globalProperties.$db = db;
 app.config.globalProperties.$auth = auth;
 app.use(router);
 
-app.mount("#app");
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: "mdi", // This is already the default value - only for display purposes
+    },
+    theme: {
+        themes: {
+            dark: {
+                light:false,
+                colors: {
+                    primary: colors.indigo.base,
+                    secondary: colors.indigo.lighten5,
+                    accent: colors.green.darken1,
+                    warning : colors.red.accent4,
+                }
+            },
+            light: {
+                dark:false,
+                colors: {
+                    primary: colors.indigo.base,
+                    secondary: colors.indigo.lighten5,
+                    accent: colors.green.darken1,
+                    warning : colors.red.accent4,
+                }
+            },
+        },
+    },
+})
+
+app.use(vuetify)
+
+
+
+app.component('UpdateMaterial',UpdateMaterial)
+app.component('CreateMaterial',CreateMaterial)
+app.component('DeleteMaterial', DeleteMaterial)
+app.component('BookMaterial',BookMaterial)
+
+app.mount('#app')
